7b2f27f4d1cee3c5f9e2eaf9f10942c193b30181
jerast <jose.romero.9602@outlook.com>
2021-07-17 16:03:12 -0500

    Se agregó la carpeta Test

---
+++ test/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<p>Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Nobis maiores accusamus, nisi voluptas quibusdam facere iure perspiciatis corporis dolorum placeat. Asperiores doloribus facere, eaque ex ullam fuga sit necessitatibus modi corrupti enim, nam beatae nihil expedita quod consequuntur magnam culpa repellat? Cumque deleniti quaerat tempora qui modi voluptatem blanditiis pariatur.</p>
+</body>
+</html>
